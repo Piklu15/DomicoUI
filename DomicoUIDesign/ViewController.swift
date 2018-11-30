de7e4425c0c96c4b608cf5db447c082ba85dfc73
Piklu Majumder <piklu.majumder@bjitgroup.com>
2018-11-30 10:51:05 +0600

    Project set up for DomicoUIDesign

---
+++ DomicoUIDesign/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DomicoUIDesign
+//
+//  Created by Piklu Majumder-401 on 11/30/18.
+//  Copyright © 2018 Piklu Majumder-401. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
